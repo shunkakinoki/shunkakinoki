a863f876baa25707050664c7a04c2fc8b4911cd1
Shun Kakinoki <shunkakinoki@gmail.com>
2020-03-14 17:47:03 +0900

    fix: 🐛 test

---
+++ coding/src/hackerrank/stl/print_pretty.cpp
@@ -0,0 +1,32 @@
+// Copyright 2019 Shun Kakinoki.
+// Reference: https://www.hackerrank.com/challenges/prettyprint/forum
+
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+  cout << setiosflags(ios::uppercase);
+  cout << setw(0xf) << internal;
+  while (T--) {
+    double A;
+    cin >> A;
+    double B;
+    cin >> B;
+    double C;
+    cin >> C;
+
+    cout << hex << left << showbase << nouppercase;
+    cout << (long long)A << endl;
+
+    cout << dec << right << setw(15) << setfill('_') << showpos << fixed
+         << setprecision(2);
+    cout << B << endl;
+
+    cout << scientific << uppercase << noshowpos << setprecision(9);
+    cout << C << endl;
+  }
+  return 0;
+}
---
+++ coding/try/hackerrank/stl/print_pretty.cpp
@@ -0,0 +1,23 @@
+// Copyright 2019 Shun Kakinoki.
+
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+  cout << setiosflags(ios::uppercase);
+  cout << setw(0xf) << internal;
+  while (T--) {
+    double A;
+    cin >> A;
+    double B;
+    cin >> B;
+    double C;
+    cin >> C;
+
+    /* Enter your code here */
+  }
+  return 0;
+}

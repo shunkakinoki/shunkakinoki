68af5028b710758be1a60e889071fec2840dfd27
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-16 14:41:59 -0800

    feat: ini main rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use clap::{crate_version, App};
+use webbrowser;
+
+fn main() {
+    let matches = App::new("shunkakinoki")
+        .about("Shun Kakinoki's CLI")
+        .version(crate_version!())
+        .author("Shun Kakinoki <shunkakinoki@gmail.com>")
+        .subcommand(App::new("home").about("Opens shunkakinoki.com"))
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("home", _)) => webbrowser::open("https://blog.shunkakinoki.com").is_ok(),
+        None => println!("No subcommand was used"),
+        _ => unreachable!(),
+    }
+}

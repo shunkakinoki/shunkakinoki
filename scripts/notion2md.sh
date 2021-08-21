3aae669c399f8db701c1e2b3fd693ca82098af0f
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-21 14:26:53 -0700

    chore: update

--- scripts/notion2md.sh
+++ scripts/notion2md.sh
@@ -4,14 +4,17 @@ yarn run notion2md --id 779208b885ac41c7913d0c3e7bb97ae6 -f about/en/ABOUT.md
 yarn run notion2md --id a95efa449e314eefaae7557695b06003 -f cause/en/CAUSE.md
 yarn run notion2md --id 8db37d862bdd4e9eb0723c14de4ca0c5 -f mission/en/MISSION.md
 yarn run notion2md --id d9f72066ffeb4b06aa48c4f27678ade5 -f values/en/VALUES.md
+yarn run notion2md --id 3db03a7ad9194999b32d5f33687ef7c7 -f history/en/HISTORY.md
 yarn run notion2md --id 05c6cb937b9840e9b2cbd5bd967aad0f -f CREDITS.md
 
 yarn run notion2md --id 1e282a0dc8ac4aa3a311027b943b200c -f about/ja/ABOUT.md
 yarn run notion2md --id 748fb4a50dc94ababcb15a4376405022 -f cause/ja/CAUSE.md
 yarn run notion2md --id a53125f645464bc6aed2ddb0fd29f82d -f mission/ja/MISSION.md
+yarn run notion2md --id cc7c793f150a4355a7cbd1e6bd082805 -f history/ja/HISTORYS.md
 yarn run notion2md --id 40db409b208d4134a29710eccd0c8fa3 -f values/ja/VALUES.md
 
 yarn run notion2md --id 1a0ae715baf5459db23daf07b208d9ef -f about/zh/ABOUT.md
 yarn run notion2md --id f100c1d222a5401aaf1a897b53763519 -f cause/zh/CAUSE.md
 yarn run notion2md --id a554db2d6f11472bbddec998f5a5f46b -f mission/zh/MISSION.md
+yarn run notion2md --id 606f2e9035f94818b33e1235e5071f02 -f history/zh/HISTORY.md
 yarn run notion2md --id 7b999c038a4544cdacf93b39c182e22e -f values/zh/VALUES.md
